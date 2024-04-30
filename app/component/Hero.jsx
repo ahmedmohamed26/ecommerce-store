38b738a3df02ebe3b57e2ce3096f66d3804a6fb4
ahmedmohamed26 <ahmedmohamed26>
2024-04-30 17:33:56 +0300

    fix hero section ui

--- app/component/Hero.jsx
+++ app/component/Hero.jsx
@@ -3,13 +3,12 @@ import React from "react";
 export const Hero = () => {
   return (
     <section className="bg-gray-50">
-      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex  lg:items-center">
+      <div className="mx-auto max-w-screen-xl px-4 pt-48 pb-16 lg:flex  lg:items-center">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl text-secondary font-extrabold sm:text-5xl">
             Understand User Flow.
             <strong className="font-extrabold  text-secondary text-red-700 sm:block">
-              {" "}
-              Increase Conversion.{" "}
+              Increase Conversion.
             </strong>
           </h1>
 
